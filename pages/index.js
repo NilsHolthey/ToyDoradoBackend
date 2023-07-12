2827e593829cf384d4e52f726308d3d572ea6bbe
Nils Holthey <nils.holthey@gmx.de>
2023-07-12 13:06:28 +0200

    update nav

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
   if (!session) {
     return (
-      <div className='bg-blue-800 w-screen h-screen flex items-center'>
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
         <div className='text-center w-full'>
           <button
             onClick={() => signIn("google")}
@@ -17,16 +18,17 @@ export default function Home() {
     );
   }
   return (
-    <div className='bg-blue-800 w-screen h-screen flex items-center'>
-      <div className='text-center w-full'>
-        <div className='mb-2'>logged in {session.user.email}</div>
-        <button
-          onClick={() => signOut()}
-          className='bg-white font-bold p-2 rounded-lg'
-        >
-          Sign out
-        </button>
+    <div className='bg-blue-900 w-screen min-h-screen flex'>
+      <Nav />
+      <div className='bg-gray-50 flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
+        logged in {session.user.email}
       </div>
+      {/* <button
+        onClick={() => signOut()}
+        className='bg-white font-bold p-3 rounded-lg'
+      >
+        Abmelden
+      </button> */}
     </div>
   );
 }

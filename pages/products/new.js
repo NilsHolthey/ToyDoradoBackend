ee97a61f2b340b001a10469ae74c0b20665552c3
Nils Holthey <nils.holthey@gmx.de>
2023-07-19 10:30:04 +0200

    add form

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,15 +1,37 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  function handleSubmit() {}
   return (
     <Layout>
-      <h1>Neues Produkt</h1>
-      <label>Produkt Name</label>
-      <input placeholder="Neues Produkt"></input>
-      <label>Produkt Beschreibung</label>
-      <textarea placeholder="Beschreibung"></textarea>
-      <label>Preis (in EUR)</label>
-      <input type="number" placeholder="Preis"></input>
+      <form onSubmit={handleSubmit}>
+        <h1>Neues Produkt</h1>
+        <label>Produkt Name</label>
+        <input
+          type="text"
+          placeholder="Neues Produkt"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+        <label>Produkt Beschreibung</label>
+        <textarea
+          placeholder="Beschreibung"
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
+        <label>Preis (in EUR)</label>
+        <input
+          type="number"
+          placeholder="Preis"
+          value={price}
+          onChange={(ev) => setPrice(ev.target.value)}
+        />
+        <button className="btn-primary">Speichern</button>
+      </form>
     </Layout>
   );
 }

19e091ef217d998959b76afb1e5da019c4bd284e
Nils Holthey <nils.holthey@gmx.de>
2023-08-03 21:28:03 +0200

    add aws-s3 upstream

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
@@ -32,6 +33,28 @@ export default function ProductForm({
   if (goBack) {
     router.push('/products');
   }
+
+  // async function uploadImages(ev) {
+  //   const files = ev.target?.files;
+  //   if (files?.length > 0) {
+  //     const data = new FormData();
+  //     files.forEach((file) => data.append('file', file));
+  //     const res = await axios.post('/api/upload', data);
+  //     console.log(res.data);
+  //   }
+  // }
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file);
+      }
+      const res = await axios.post('/api/upload', data);
+
+      console.log(res.data);
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Produkt Name</label>
@@ -41,6 +64,29 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Bilder</label>
+      <div className="mb-2">
+        <label className="w-32 h-32 bg-gray-200 border-dashed border-2 border-gray-400 text-center flex text-sm justify-center items-center gap-1 cursor-pointer text-gray-600 rounded-xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Hochladen</div>
+          <input type="file" className="hidden" onChange={uploadImages}></input>
+        </label>
+
+        {!images?.length && <div> keine Bilder vorhanden</div>}
+      </div>
       <label>Produkt Beschreibung</label>
       <textarea
         placeholder="Beschreibung"
